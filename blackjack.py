6b602938738686ec6511482397cabcecfd55e745
Joshua Alletto <joshalletto@gmail.com>
2018-06-22 18:45:50 -0500

    sort of functioning.

---
+++ blackjack.py
@@ -0,0 +1,92 @@
+import random 
+import os
+
+class BlackJack:
+    
+    suits = ['\u2663', '\u2666', '\u2660', '\u2665']
+
+    def __init__(self):
+        self.deck = self.make_deck()
+        self.dealer = []
+        self.player = []
+        random.shuffle(self.deck)
+        self.deal()
+
+    def make_deck(self):
+        deck = []
+        for suit in self.suits:
+            deck += [f'A{suit}',f'K{suit}', f'Q{suit}', f'J{suit}' ]
+            for i in range(2,11):
+                deck.append(f'{i}{suit}')
+        return deck 
+
+    def deal(self):
+        for _i in range(1,3):
+            self.dealer.append(self.deck.pop())
+            self.player.append(self.deck.pop())
+
+    def hand_value(self, hand):
+        values = list(map(self.card_to_value, hand))
+        total = sum(values)
+        if total > 21 and 11 in values:
+            i = values.index(11)
+            values[i] = 1
+            return sum(values)
+        return total 
+    
+    def card_to_value(self, card):
+        face_cards = ['Q', 'K', 'J', '1']
+        card_val = card[0]
+        if card_val in face_cards:
+            card_val = '10'
+        if card_val == 'A':
+            card_val = '11'
+        return int(card_val)
+   
+    def can_hit(self, hand):
+        return self.hand_value(hand) < 21
+
+    def hit(self, player):
+        player.append(self.deck.pop())
+
+    def dealer_play(self):
+        while self.hand_value(self.dealer) < 16:
+            self.hit(self.dealer)
+
+    def get_winner(self):
+        p = self.hand_value(self.player)
+        d = self.hand_value(self.dealer)
+        if p == 21 or p < 21 and p > d or d > 21:
+            print('player wins')
+        elif p > 21 or d < 21 and d > p:
+            print('dealer wins')
+        elif d == p:
+            print('tie game.')
+
+
+    def game_state(self):
+        print('DEALER\n')
+        self.display_hand(self.dealer)
+        print('PLAYER\n')
+        self.display_hand(self.player)
+    
+    def display_hand(self, hand):
+        string = ""
+        for card in hand:
+            string += f'| {card} |   '
+        print(string +' \n')
+
+    
+    def play(self):
+        self.game_state()
+        player_choice = input("S to stay. H to hit.\n").lower()
+        while player_choice == "h":
+            os.system('clear')
+            self.hit(self.player)
+            self.game_state()
+            player_choice = input("S to stay. H to hit.\n").lower()
+
+        os.system('clear')
+        self.dealer_play()
+        self.game_state()
+        self.get_winner()
---
+++ runner.py
@@ -0,0 +1,10 @@
+from blackjack import BlackJack
+
+BlackJack().play()
+
+
+# after each player play 
+    # does player have 21? 
+    # player wins 
+    # does player bust? 
+    # dealer wins 
