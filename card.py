2875b5c74c9da20445466de5ec5fc90c91cf0ec7
Joshua Alletto <joshalletto@gmail.com>
2018-08-24 20:34:39 -0500

    wip

--- card.py
+++ card.py
@@ -1,10 +1,10 @@
 class Card: 
 
-    card_back = '| \\\\ |'
+    card_back = '|  \\\\  | '
     def __init__(self, suit, value, face=None):
         self.suit = suit
         self.face = face  
         self.value = value 
 
     def __str__(self):
-        return f'| { self.face if self.face else self.value }{self.suit}  | '
+        return f'| { self.face if self.face else self.value }{self.suit}   | '
